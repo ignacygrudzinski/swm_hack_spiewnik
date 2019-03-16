eacd9dcdca073bbb923a698f484c78c986acc4fc
ignacygrudzinski <ignacy.grudzinski@gmail.com>
2019-03-16 12:08:39 +0100

    made song entry i guess

---
+++ components/SongEntry.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Alert, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+
+export default class SongEntry extends Component {
+    _onPressButton() {
+        Alert.alert('You tapped the button!')
+    }
+
+
+    render() {
+        return (
+                <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+                    <View style={styles.entry}>
+                        <Text style={styles.titleText}>{this.props.title}</Text>
+                        <Text style={styles.authorText}>{this.props.author}</Text>
+                    </View>
+                </TouchableHighlight>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+    entry: {
+        paddingTop: 10,
+        width: '100%',
+        alignItems: 'flex-start',
+        paddingBottom: 10,
+        borderBottomColor: 'gray',
+        borderBottomWidth: 1
+    },
+    titleText: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    authorText: {
+        fontSize: 10,
+    }
+
+});
